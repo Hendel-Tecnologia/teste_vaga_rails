b67cf3fcd024df5d42ab29ec6e55f248311c237b
Wallas Faria <wallasfaria@hotmail.com>
2021-07-06 00:05:15 -0300

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 Faker::Config.locale = 'pt-BR'
 
 Product.create!(
-  (1..100).map do
+  (1..500).map do
     {
-      name: Faker::Commerce.product_name,
+      name: Faker::Commerce.unique.product_name,
       description: Faker::Lorem.sentences.join,
       price: Faker::Commerce.price,
       quantity: Faker::Number.between(from: 0, to: 50)
