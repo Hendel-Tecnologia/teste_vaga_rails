6e9a057be7a1dce9b560cc903bef3d9ba6c8184d
Wallas Faria <wallasfaria@hotmail.com>
2021-07-07 01:27:06 -0300

    add default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products, defaults: { format: :json }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
