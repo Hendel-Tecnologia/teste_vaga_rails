e73c1c7da497f62aea87a77dab61c23c68a5ee81
Wallas Faria <wallasfaria@hotmail.com>
2021-07-07 01:32:02 -0300

    add json format in spec routes

--- spec/routing/products_routing_spec.rb
+++ spec/routing/products_routing_spec.rb
@@ -3,27 +3,27 @@ require 'rails_helper'
 RSpec.describe ProductsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/products').to route_to('products#index')
+      expect(get: '/products').to route_to('products#index', format: :json)
     end
 
     it 'routes to #show' do
-      expect(get: '/products/1').to route_to('products#show', id: '1')
+      expect(get: '/products/1').to route_to('products#show', id: '1', format: :json)
     end
 
     it 'routes to #create' do
-      expect(post: '/products').to route_to('products#create')
+      expect(post: '/products').to route_to('products#create', format: :json)
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/products/1').to route_to('products#update', id: '1')
+      expect(put: '/products/1').to route_to('products#update', id: '1', format: :json)
     end
 
     it 'routes to #update via PATCH' do
-      expect(patch: '/products/1').to route_to('products#update', id: '1')
+      expect(patch: '/products/1').to route_to('products#update', id: '1', format: :json)
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/products/1').to route_to('products#destroy', id: '1')
+      expect(delete: '/products/1').to route_to('products#destroy', id: '1', format: :json)
     end
   end
 end
